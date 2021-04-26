10b9ca90a6418a394bcab12bc96722e68d8fcdbb
duanhuarong <duanhuarong@huaxintec.com>
2021-04-26 17:43:11 +0800

    使用less定义常用变量

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+const vueConfig = {
+    // 加上以下的配置
+    chainWebpack: (config) => {
+        const oneOfsMap = config.module.rule("less").oneOfs.store;
+        oneOfsMap.forEach(item => {
+            item
+                .use("style-resources-loader")
+                .loader("style-resources-loader")
+                .options({
+                    // or an array : ["./path/to/vars.less", "./path/to/mixins.less"] 这里的路径不能使用@，否则会报错
+                    patterns: "./src/styles/mixin.less"
+                })
+                .end()
+        })
+    }
+}
+module.exports = vueConfig
